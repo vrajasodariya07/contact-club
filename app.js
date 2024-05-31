b873376f756af28650e8cf64b3919214a5ee765d
Denish <36415859+Denni007@users.noreply.github.com>
2024-05-31 18:45:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-// app.js
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const User = require('./models/user'); // Assuming you have a User model set up
+// // app.js
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const User = require('./models/user'); // Assuming you have a User model set up
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb://localhost:27017/test', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-app.put('/api/users/:id', async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+// app.put('/api/users/:id', async (req, res) => {
+//   try {
+//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+//       new: true,
+//       runValidators: true,
+//     });
 
-    if (!updatedUser) {
-      return res.status(404).send();
-    }
+//     if (!updatedUser) {
+//       return res.status(404).send();
+//     }
 
-    res.send(updatedUser);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+//     res.send(updatedUser);
+//   } catch (e) {
+//     res.status(400).send(e);
+//   }
+// });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
