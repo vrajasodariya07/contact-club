bf4e27d724e32ab023a641375e1495a5587b679b
Denish <36415859+Denni007@users.noreply.github.com>
2024-05-31 18:41:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/yourdbname', {
+mongoose.connect('mongodb://localhost:27017/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
